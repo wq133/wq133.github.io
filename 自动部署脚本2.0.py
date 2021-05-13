ff5d4aba862bb6039d257aa599b48d243be4de0c
wq133 <wqh666@vip.qq.com>
2021-05-13 16:44:48 +0800

    update blog

--- 自动部署脚本2.0.py
+++ 自动部署脚本2.0.py
@@ -30,19 +30,27 @@ def countdown(t):
 
 
 if __name__ == '__main__':
-    cmd1 = "git add ."
-    cmd2 = 'git commit -m "update blog"'
-    cmd3 = "git push -u origin master"
+    cmd1 = 'git pull'                       # 拉取代码
+    cmd2 = 'git add .'                      # 添加文件到git
+    cmd3 = 'git commit -m "update blog"'    # 提交备注
+    cmd4 = 'git push -u origin master'      # 推送代码
 
     time.sleep(1)
 
-    runCmd(cmd1)  # 清除public文件
+    print('执行 git pull 命令！')
+    runCmd(cmd1)        
     time.sleep(2)
 
-    runCmd(cmd2)
+    print('执行 git add . 命令！')
+    runCmd(cmd2) 
     time.sleep(2)
 
-    runCmd(cmd3)  # 上传文件
+    print('执行 git commit -m "update blog" 命令！')
+    runCmd(cmd3)
+    time.sleep(2)
+
+    print('执行 git push -u origin master 命令！')
+    runCmd(cmd4)  
     time.sleep(2)
 
     print("上传成功！！！\n")      # 提示成功
