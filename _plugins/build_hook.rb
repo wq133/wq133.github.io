a025ab91206a43f34449dc7aa5849181277f746a
wq133 <wqh666@vip.qq.com>
2024-02-25 20:15:16 +0800

    图片url替换rb

---
+++ _plugins/build_hook.rb
@@ -0,0 +1,9 @@
+
+<!-- 创建一个钩子来在构建期间修改
+ Markdown 中渲染的 HTML 图像标签来解决此问题。
+ 本质上，尝试将字符串插入标签的属性 ../article_img替换成 /article_img
+ -->
+
+Jekyll::Hooks.register :posts, :post_render do |post|
+  post.output.gsub!('<img src="../article_img', '<img src="/article_img')
+end
